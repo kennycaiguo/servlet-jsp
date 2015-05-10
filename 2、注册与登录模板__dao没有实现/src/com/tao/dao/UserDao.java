99024ffdefb9e25ee5449948fd597d6dae47efa7
shixinga <qqchenshixing@163.com>
2015-05-10 23:29:07 +0800

    servlet
    项目上传

---
+++ 2、注册与登录模板__dao没有实现/src/com/tao/dao/UserDao.java
@@ -0,0 +1,29 @@
+package com.tao.dao;
+
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+
+import com.tao.model.User;
+
+/**
+ * 数据类
+ *
+ */
+public class UserDao {
+	/**
+	 * 按用户名查询
+	 * @param username
+	 * @return
+	 */
+	public User findByUsername(String username) {
+		return null;
+	}
+	
+	/**
+	 * 添加用户
+	 * @param user
+	 */
+	public void add(User user) {
+	}
+}
